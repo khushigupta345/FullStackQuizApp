cf62e0b98adde73de590147036c19552d4971a9e
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-07-04 02:53:27 +0530

    Update admin-routing.module.ts

--- frontend/angular-starter-project-master/src/app/module/admin/admin-routing.module.ts
+++ frontend/angular-starter-project-master/src/app/module/admin/admin-routing.module.ts
@@ -4,66 +4,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateTestComponent } from './components/create-test/create-test.component';
 import { AddQuestionQuizComponent } from './components/add-question-quiz/add-question-quiz.component';
 import { ViewQuestionComponent } from './components/view-question/view-question.component';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { QuizService } from 'src/app/auth/signup/quiz.service';
 import { ViewResultQuizComponent } from './view-result-quiz/view-result-quiz.component';
 import { EditquizComponent } from './components/editquiz/editquiz.component';
 
-
-
-
 const routes: Routes = [
-
-    { path:"dashboard",component:DashboardComponent},
-  
-   { path:"create-test",component:CreateTestComponent},
-   { path:"add-question/:id",component:AddQuestionQuizComponent},
-   { path:"view-test/:id",component:ViewQuestionComponent}
-   ,{ path:"view-test-result",component:ViewResultQuizComponent}
-   ,{ path:"edit-quiz/:id",component:EditquizComponent}
-   
+  { path: "dashboard", component: DashboardComponent },
+  { path: "create-test", component: CreateTestComponent },
+  { path: "add-question/:id", component: AddQuestionQuizComponent },
+  { path: "view-test/:id", component: ViewQuestionComponent },
+  { path: "view-test-result", component: ViewResultQuizComponent },
+  { path: "edit-quiz/:id", component: EditquizComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { 
-quizzes=[];
-  constructor(
-    private notification: NzNotificationService,
-    private quizService: QuizService
-  ) {}
-
-  ngOnInit() {
-    this.getQuizzes();
-  }
-
-  getQuizzes() {
-    this.quizService.getAllQuiz().subscribe(
-      (res) => {
-        this.quizzes = res;
-      },
-      (error) => {
-        this.notification.error(
-          'ERROR',
-          'Something went wrong',
-          { nzDuration: 54000 }
-        );
-      }
-    );
-  }
-
-  // Corrected time formatting function
-  getFormattedTime(time: number): string {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes} minutes ${seconds} seconds`;
-  }
-
-  submitQuiz() {
-    // Implement your logic for "View Test" or "Add Question"
-  }
-}
-
-
+export class AdminRoutingModule { }
