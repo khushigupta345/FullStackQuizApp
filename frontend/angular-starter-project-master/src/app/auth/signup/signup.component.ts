d47db92f6bdadf1f2d87f3f2915e7959f2289735
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-07-05 20:55:53 +0530

    Update signup.component.ts

--- frontend/angular-starter-project-master/src/app/auth/signup/signup.component.ts
+++ frontend/angular-starter-project-master/src/app/auth/signup/signup.component.ts
@@ -1,49 +1,4 @@
-// import { Component } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { NzMessageService } from 'ng-zorro-antd/message';
-// import { SharedModule } from 'src/app/module/shared/shared.module';
-// import { QuizService } from './quiz.service';
 
-// @Component({
-//   selector: 'app-signup',
-//   templateUrl: './signup.component.html',
-//   styleUrls: ['./signup.component.css']
-// })
-// export class SignupComponent {
-
-  
-//   constructor(private fb:FormBuilder ,private router: Router,private message:NzMessageService,private quizsevice:QuizService){}
-//   validateForm!:FormGroup;
-//   ngOnInit(){
-//     this.validateForm=this.fb.group({
-//       email:[null,[Validators.required,Validators.email]],
-//       name:[null,[Validators.required]],
-//       password:[null,[Validators.required]]
-//     })
-//   }
-
-//   submitForm(){
-//     this.quizsevice.register(this.validateForm.value).subscribe(res=>{
-// this.message
-// .success(`signup success`,
-// {
-  
-
-//  this.  router.navigate(['/training-info']);
-
-// }
-// );
-//     },eroor=>{
-//       this.message
-//       .error(
-//         `Bad Credentials`,
-//         {nzDuration:5000}
-//         );
-      
-//     })
-//   }
-//   }
 
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
