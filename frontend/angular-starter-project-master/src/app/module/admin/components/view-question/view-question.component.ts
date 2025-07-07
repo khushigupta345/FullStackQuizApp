ae44ad2c24c48b5988a34b9735ef3abdea0a954e
Khushi Gupta <148431685+khushigupta345@users.noreply.github.com>
2025-07-07 13:50:17 +0530

    Update view-question.component.ts

--- frontend/angular-starter-project-master/src/app/module/admin/components/view-question/view-question.component.ts
+++ frontend/angular-starter-project-master/src/app/module/admin/components/view-question/view-question.component.ts
@@ -8,24 +8,43 @@ import { QuizService } from 'src/app/auth/signup/quiz.service';
   styleUrls: ['./view-question.component.css']
 })
 export class ViewQuestionComponent {
-questions :any[]=[];
-testId :any;
-constructor (private ac: ActivatedRoute,
-   
-    private quizService: QuizService){}   
-    ngOnInit(){   
-      this.ac.paramMap.subscribe(param=>{
-        this.testId=+param.get('id');
-        // this.quizService.getAllQuestion(this.testId).subscribe(
-          this.quizService.getAllQuestionAdmin(this.testId).subscribe(
-          res=>{
-            
-            this.questions=res.questions;
-            console.log(this.questions);
+  questions: any[] = [];
+  testId: any;
+  selectedAnswers: { [key: number]: string } = {};
+  submitted: boolean = false;
+  score: number = 0;
+  pass: boolean = false;
 
-          }
-        )
-      
- } )}
+  constructor(
+    private ac: ActivatedRoute,
+    private quizService: QuizService
+  ) {}
 
+  ngOnInit() {
+    this.ac.paramMap.subscribe(param => {
+      this.testId = +param.get('id');
+      this.quizService.getAllQuestionAdmin(this.testId).subscribe(res => {
+        this.questions = res.questions;
+        console.log(this.questions);
+      });
+    });
+  }
+
+  selectAnswer(questionId: number, option: string) {
+    this.selectedAnswers[questionId] = option;
+  }
+
+  submitAnswers() {
+    this.submitted = true;
+    this.score = 0;
+
+    this.questions.forEach(q => {
+      if (this.selectedAnswers[q.id] === q.correctOption) {
+        this.score++;
+      }
+    });
+
+    // Optional: Pass/Fail logic (pass if >= 50%)
+    this.pass = (this.score / this.questions.length) * 100 >= 50;
+  }
 }
